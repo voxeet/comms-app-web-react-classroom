78591d31e869b9b4a9f8ed49f51516ee77209d10
Griffin <19497824+WingofaGriffin@users.noreply.github.com>
2022-10-04 15:21:37 -0700

    update packages

--- src/providers/Firebase.js
+++ src/providers/Firebase.js
@@ -1,5 +1,5 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
 
 const firebaseConfig = {
   apiKey: "<API_KEY>",
